67cb4719c953a8bbb432da5e5a178ab2f47161e4
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2024-09-13 20:35:36 +0900

    first import

---
+++ go.mod
@@ -0,0 +1,29 @@
+module github.com/mattn/nostr-saizeriya-gacha
+
+go 1.23.0
+
+require (
+	github.com/PuerkitoBio/goquery v1.10.0
+	github.com/nbd-wtf/go-nostr v0.35.0
+)
+
+require (
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/btcsuite/btcd/btcec/v2 v2.3.2 // indirect
+	github.com/btcsuite/btcd/btcutil v1.1.3 // indirect
+	github.com/btcsuite/btcd/chaincfg/chainhash v1.0.2 // indirect
+	github.com/decred/dcrd/crypto/blake256 v1.0.1 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.2.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/puzpuzpuz/xsync/v3 v3.0.2 // indirect
+	github.com/tidwall/gjson v1.14.4 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+	golang.org/x/exp v0.0.0-20230425010034-47ecfdc1ba53 // indirect
+	golang.org/x/net v0.29.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"bytes"
+	_ "embed"
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/nbd-wtf/go-nostr"
+	"github.com/nbd-wtf/go-nostr/nip19"
+)
+
+const name = "nostr-saizeriya-gacha"
+
+const version = "0.0.0"
+
+var revision = "HEAD"
+
+type Item struct {
+	ID    string `json:"id"`
+	Name  string `json:"name"`
+	Price int    `json:"price"`
+	Note  string `json:"note"`
+}
+
+var (
+	//go:embed menu.json
+	menujson []byte
+
+	menu []Item
+)
+
+func init() {
+	err := json.Unmarshal(menujson, &menu)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handler(nsec string) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			return
+		}
+		var ev nostr.Event
+		err := json.NewDecoder(r.Body).Decode(&ev)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		tok := strings.Split(ev.Content, " ")
+		price := 1000
+		if len(tok) == 2 {
+			price, _ = strconv.Atoi(tok[1])
+		}
+		if price < 0 {
+			price = 1000
+		}
+
+		founds := []Item{}
+
+		for {
+			rand.Shuffle(len(menu), func(i, j int) {
+				menu[i], menu[j] = menu[j], menu[i]
+			})
+
+			hit := -1
+			for i, m := range menu {
+				if m.Price <= price {
+					price -= m.Price
+					hit = i
+					break
+				}
+			}
+			if hit == -1 {
+				break
+			}
+			founds = append(founds, menu[hit])
+		}
+
+		var buf bytes.Buffer
+		for _, m := range founds {
+			fmt.Fprintf(&buf, "%s: %s %d円\n", m.ID, m.Name, m.Price)
+		}
+		fmt.Fprintf(&buf, "\n#サイゼリヤガチャ")
+
+		eev := nostr.Event{}
+		var sk string
+		if _, s, err := nip19.Decode(nsec); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		} else {
+			sk = s.(string)
+		}
+		if pub, err := nostr.GetPublicKey(sk); err == nil {
+			if _, err := nip19.EncodePublicKey(pub); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+			eev.PubKey = pub
+		} else {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		eev.Content = buf.String()
+		eev.CreatedAt = nostr.Now()
+		eev.Kind = ev.Kind
+		eev.Tags = eev.Tags.AppendUnique(nostr.Tag{"e", ev.ID, "", "reply"})
+		for _, te := range ev.Tags {
+			if te.Key() == "e" {
+				eev.Tags = eev.Tags.AppendUnique(te)
+			}
+		}
+		eev.Tags = eev.Tags.AppendUnique(nostr.Tag{"t", "サイゼリヤガチャ"})
+		eev.Sign(sk)
+
+		w.Header().Set("content-type", "text/json; charset=utf-8")
+		json.NewEncoder(w).Encode(eev)
+	}
+}
+
+func main() {
+	nsec := os.Getenv("BOT_NSEC")
+	if nsec == "" {
+		log.Fatal("BOT_NSEC is not set")
+	}
+
+	http.HandleFunc("/", handler(nsec))
+
+	addr := ":" + os.Getenv("PORT")
+	if addr == ":" {
+		addr = ":8080"
+	}
+	log.Printf("started %v", addr)
+	http.ListenAndServe(addr, nil)
+}
